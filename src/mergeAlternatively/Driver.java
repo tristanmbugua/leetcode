a72710ecd32bafc77a5ec47b59bc83fb4b15680d
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2024-03-09 11:46:46 -0500

    Leetcode Problem - Merge Strings Alternately
    This problem was solved February 14th 2024 and updated March 9th 2024.

---
+++ src/mergeAlternatively/Driver.java
@@ -0,0 +1,48 @@
+package mergeAlternatively;
+
+/**
+ * <pre>
+ * Runtime: 2ms
+ * Runtime Rank: Beats 32.25% of users with Java 
+ * Memory: 42.78MB
+ * Memory Rank: Beats 15.33% of users with Java
+ * <pre>
+ * @version Date: March 9th 2024
+ * @author Tristan Mbugua
+ */
+public class Driver {
+	public static void main(String[] args) {
+		System.out.println(mergeAlternately("abc", "pqr"));
+	}
+
+	public static String mergeAlternately(String word1, String word2) {
+		int fullLength = word1.length() + word2.length();
+        String newWord = "";
+        for (int i = 0; i < fullLength; i++) {
+            if (word1.length() > 0) {
+            	if (word2.length() > 0) {
+            		if ((i % 2) == 0) {
+            			newWord += word1.substring(0, 1);
+            			word1 = word1.substring(1);
+            		} else {
+            			newWord += word2.substring(0, 1);
+            			word2 = word2.substring(1);
+            		}
+            	} else {
+            		newWord += word1;
+            		return newWord;
+            	}
+        	} else {
+        		newWord += word2;
+        		return newWord;
+        	}
+        }
+        return newWord;
+	}
+}
+
+/* Shorter answer:
+ * for (int i = 0; ((word1.length() > i) && (word2.length() > 0)); i=i+2) {
+ * word1 = word1.substring(0, i + 1) + word2.substring(0, 1) + word1.substring(i
+ * + 1); word2 = word2.substring(1); } word1 += word2; return word1;
+ */
