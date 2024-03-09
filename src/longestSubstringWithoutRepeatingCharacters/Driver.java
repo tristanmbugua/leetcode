61af8c56878c868c53294a5950e8d3352214f3cc
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2024-03-09 11:00:11 -0500

    Updated LongestSubstringWithoutRepeatingCharacters.
    Added leetcode stats.

--- src/longestSubstringWithoutRepeatingCharacters/Driver.java
+++ src/longestSubstringWithoutRepeatingCharacters/Driver.java
@@ -1,7 +1,12 @@
 package longestSubstringWithoutRepeatingCharacters;
 
 /**
- * 
+ * Date: March 8th 2024
+ * Runtime: 396ms
+ * Runtime Rank: Beats 5.00% of users with Java
+ * Memory: 45.27MB
+ * Memory Rank: Beats 14.57% of users with Java
+ * @author Tristan Mbugua
  */
 public class Driver {
 
@@ -9,22 +14,21 @@ public class Driver {
 		System.out.println(LengthOfLongestSubstring("aab"));
 	}
 	
+	/**
+	 * Find the longest substring in a string.
+	 * @param s String to be searched.
+	 * @return The length of the longest substring in a string.
+	 */
 	public static int LengthOfLongestSubstring(String s) {
         String usedCharacters = "";
-        Boolean alarm = false;
-        String newString = "";
         int returnVal = 0;
         for (int i = 0; i < s.length(); i++) {
         	if (usedCharacters.replace(s.charAt(i), 'შ').equals(usedCharacters)) {
         		usedCharacters += s.substring(i, (i+1));
-        		
-            	newString+=s.substring(i, (i+1));
-            	if (newString.length() > returnVal) {
-            		returnVal = newString.length();
+        		if (usedCharacters.length() > returnVal) {
+            		returnVal = usedCharacters.length();
             	}
-            	
             } else {
-            	newString = "";
     			usedCharacters = "";
     			s = s.substring(1);
     			i = -1;
